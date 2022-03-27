9fd0723514f5c2aec4e9b3506a6973f4b73f3e01
Sam O <sam.odukoya@jdplc.com>
2022-03-27 14:10:23 +0100

    refactor code

--- issue2/lib/date_range_formatter.rb
+++ issue2/lib/date_range_formatter.rb
@@ -13,46 +13,42 @@ class DateRangeFormatter
     full_start_date = @start_date.strftime("#{@start_date.day.ordinalize} %B %Y")
     full_end_date = @end_date.strftime("#{@end_date.day.ordinalize} %B %Y")
 
+    # Checks if the start and end dates are the same
     if @start_date == @end_date
       if @start_time && @end_time
-        "#{full_start_date} at #{@start_time} to #{@end_time}"
+        return "#{full_start_date} at #{@start_time} to #{@end_time}"
       elsif @start_time
-        "#{full_start_date} at #{@start_time}"
+        return "#{full_start_date} at #{@start_time}"
       elsif @end_time
-        "#{full_start_date} until #{@end_time}"
+        return "#{full_start_date} until #{@end_time}"
       else
-        full_start_date
-      end
-    elsif @start_date.month == @end_date.month
-      if @start_time && @end_time
-        "#{full_start_date} at #{@start_time} - #{full_end_date} at #{@end_time}"
-      elsif @start_time
-        "#{full_start_date} at #{@start_time} - #{full_end_date}"
-      elsif @end_time
-        "#{full_start_date} - #{full_end_date} at #{@end_time}"
-      else
-        @start_date.strftime("#{@start_date.day.ordinalize} - #{@end_date.day.ordinalize} %B %Y")
-      end
-    elsif @start_date.year == @end_date.year
-      if @start_time && @end_time
-        "#{full_start_date} at #{@start_time} - #{full_end_date} at #{@end_time}"
-      elsif @start_time
-        "#{full_start_date} at #{@start_time} - #{full_end_date}"
-      elsif @end_time
-        "#{full_start_date} - #{full_end_date} at #{@end_time}"
-      else
-        @start_date.strftime("#{@start_date.day.ordinalize} %B - ") + @end_date.strftime("#{@end_date.day.ordinalize} %B %Y")
-      end
-    else
-      if @start_time && @end_time
-        "#{full_start_date} at #{@start_time} - #{full_end_date} at #{@end_time}"
-      elsif @start_time
-        "#{full_start_date} at #{@start_time} - #{full_end_date}"
-      elsif @end_time
-        "#{full_start_date} - #{full_end_date} at #{@end_time}"
-      else
-        "#{full_start_date} - #{full_end_date}"
+        return full_start_date
       end
     end
+
+    #  Checks if start or end times are set
+    if @start_time && @end_time
+      return "#{full_start_date} at #{@start_time} - #{full_end_date} at #{@end_time}"
+    elsif @start_time
+      return "#{full_start_date} at #{@start_time} - #{full_end_date}"
+    elsif @end_time
+      return "#{full_start_date} - #{full_end_date} at #{@end_time}"
+    end
+
+    # Checks if the start and end months are the same
+    if @start_date.month == @end_date.month
+      if @start_date.year == @end_date.year
+        return @start_date.strftime("#{@start_date.day.ordinalize} - #{@end_date.day.ordinalize} %B %Y")
+      end
+
+      return @start_date.strftime("#{@start_date.day.ordinalize} %B %Y") + " - " + @end_date.strftime("#{@end_date.day.ordinalize} %B %Y")
+    end
+
+    # Checks if the start and end years are the same
+    if @start_date.year == @end_date.year
+      return @start_date.strftime("#{@start_date.day.ordinalize} %B") + " - " + @end_date.strftime("#{@end_date.day.ordinalize} %B %Y")
+    end 
+
+    @start_date.strftime("#{@start_date.day.ordinalize} %B %Y") + " - " + @end_date.strftime("#{@end_date.day.ordinalize} %B %Y")
   end
 end
--- issue2/spec/date_range_formatter_spec.rb
+++ issue2/spec/date_range_formatter_spec.rb
@@ -61,8 +61,8 @@ RSpec.describe(DateRangeFormatter) do
     expect(formatter.to_s).to eq('1st November 2009 at 10:00 - 1st December 2010 at 11:00')
   end
 
-  # it 'formats a date range for same month with different year' do
-  #   formatter = DateRangeFormatter.new('2009-11-1', '2010-11-3')
-  #   expect(formatter.to_s).to eq('1st November 2009 - 3rd November 2010')
-  # end
+  it 'formats a date range for same month with different year' do
+    formatter = DateRangeFormatter.new('2009-11-1', '2010-11-3')
+    expect(formatter.to_s).to eq('1st November 2009 - 3rd November 2010')
+  end
 end
